d3f84234e16688cb0fa398038e32676f820c75ba
Jake Prins <jakeprins@outlook.com>
2021-09-24 14:38:40 +0200

    Add sandbox mode to settings on redirect to vault

--- src/utils/createVaultSession.ts
+++ src/utils/createVaultSession.ts
@@ -1,7 +1,10 @@
 export const createVaultSession = async (url?: string) => {
   const response = await fetch('/api/vault/sessions', {
     method: 'POST',
-    body: JSON.stringify({ redirect_uri: url || window.location.href })
+    body: JSON.stringify({
+      redirect_uri: url || window.location.href,
+      settings: { sandbox_mode: true }
+    })
   })
   return response.json()
 }
